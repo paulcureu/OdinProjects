ee1c8eb731ae2baf4eda38fa322ef790f8f1fe65
Paul <cureu.paul.ioan@gmail.com>
2025-05-01 03:36:55 +0300

    done

--- Library/main.js
+++ Library/main.js
@@ -1,5 +1,5 @@
-const divAddBook = document.getElementById("showOrHide");
-let isClicked = true;
+document.getElementById("new-book").style.display = "none";
+let isCliked = false;
 const myLibrary = [];
 
 function Book(title, author, pages, readIt) {
@@ -10,21 +10,51 @@ function Book(title, author, pages, readIt) {
     this.readIt = readIt;
 }
 
-function addBook(title) {
-    myLibrary.push(title);
-}
-
-divAddBook.style.display = "none";
-let showOrHide = function (){
-    if(isClicked){
-        divAddBook.style.display = 'block';
-        isClicked = false;
-    }else{
-        divAddBook.style.display = 'none';
-        isClicked = true;
+function rander() {
+    let libraryElement = document.getElementById('library');
+    libraryElement.innerHTML = '';
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+        let bookElement = document.createElement('div');
+        bookElement.classList.add('book');
+        bookElement.innerHTML = '<div style = "display: flex; justify-content: center">'+'<p>Book title: </p>'+book.title+'</div>'
+            +'<div style = "display: flex; justify-content: center">'+'<p>Author: </p>'+book.author+'</div>'
+            +'<div style = "display: flex; justify-content: center">'+'<p>Pages: </p>'+book.pages+'</div>'
+            +'<div style = "display: flex; justify-content: center">'+'<p>Read It: </p>'+(book.readIt ? "yes":"no")+'</div>'
+            +'<button class="btn-book" data-id="' + book.id + '">Delete</button>'
+        libraryElement.appendChild(bookElement);
+        bookElement.querySelector(".btn-book").addEventListener("click", function () {
+            const id = this.getAttribute("data-id");
+            const index = myLibrary.findIndex(book => book.id === id);
+            if (index !== -1) {
+                myLibrary.splice(index, 1);
+                rander();
+            }
+        });
     }
-
 }
 
-const divLibrary = document.getElementById("showOrHideLibrary");
-divLibrary.style.display = "grid";
+function addBookToLibrary() {
+    let title = document.getElementById("bookTitle").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let readIt = document.getElementById("readIt").checked;
+    let newBook = new Book(title, author, pages, readIt, readIt, readIt);
+    myLibrary.push(newBook);
+    rander();
+}
+
+document.querySelector(".btn").addEventListener("click", function (e) {
+    if (isCliked) {
+        document.getElementById("new-book").style.display = "none";
+        isCliked = false;
+    }else {
+        document.getElementById("new-book").style.display = "block";
+        isCliked = true;
+    }
+});
+
+document.querySelector("#new-book").addEventListener("submit", function (event) {
+    event.preventDefault();
+    addBookToLibrary();
+})
